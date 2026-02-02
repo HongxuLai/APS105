215b8cb01fa6c4932f0b064caeae0ed1c1373c97
Hongxu Lai <hongxu.lai66@gmail.com>
2026-02-01 19:04:55 -0500

    change a sign

--- lab4/lab4part2.c
+++ lab4/lab4part2.c
@@ -21,7 +21,7 @@ int main()
             if (lastnum == 7) threetimes += 1;
             restnum /= 10;
         }
-        if (threetimes == 3) times += 1;
+        if (threetimes >= 3) times += 1;
     } while(num != 0);
     printf("You entered %d pretty number(s)!\n", times);
     return 0;
